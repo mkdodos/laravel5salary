b352623f8b449e6e3f2a6379e74e5637584a3571
mk <mkdodos@gmail.com>
2022-04-28 11:08:26 +0800

    footer

--- resources/views/emp/basic.blade.php
+++ resources/views/emp/basic.blade.php
@@ -1,3 +1,5 @@
+@extends('app')
+@section('content')
 <div id="app" class="container">
     <table class="display" id="table_id">
         <thead>
@@ -17,9 +19,11 @@
 
 
 </div>
+@endsection
+@section('footer')
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
 <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
@@ -45,3 +49,4 @@
         }
     })
 </script>
+@endsection
