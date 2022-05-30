e7416466676d21c6a11bc74d33568d5d56ce6afe
mk <mkdodos@gmail.com>
2022-05-30 18:25:56 +0800

    escape

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -1,8 +1,11 @@
-<?php namespace App\Http\Controllers;
+<?php
 
-class ExpenseController extends Controller {
+namespace App\Http\Controllers;
 
-	
+class ExpenseController extends Controller
+{
+
+
 
 
 	/**
@@ -37,19 +40,23 @@ class ExpenseController extends Controller {
 
 		
 
-		$query = " SELECT top 5 ID,日期,品名 FROM 費用表 ";
+		$query = " SELECT top 35000 ID,日期,品名 FROM 費用表 ";
+
+		// $query = " SELECT top 5 ID,日期,品名 FROM 費用表 WHERE ID=5371 ";
+		// $query = " SELECT 品名 FROM 費用表 WHERE ID=5371 ";
 
 		if ($where) {
 			$query .= $where;
 		}
 
+		// return $name;
 		// return $query;
 
 		$query = mb_convert_encoding($query, "BIG5", "UTF-8");
 		$rs = $db->query($query);
 		$arr = $rs->fetchAll(\PDO::FETCH_ASSOC);
 
-		$keys = [ 'id','date','name'];
+		$keys = ['id', 'date', 'name'];
 		$json = "";
 
 
@@ -60,24 +67,31 @@ class ExpenseController extends Controller {
 			$j = 0;
 			foreach ($arr[$i] as $key => $value) {
 				// 字串後面有空白導致無法正確輸出 json 格式, 加上 trim            
+				$value = str_replace('"','\"',$value);
 				$newarr[urlencode($keys[$j])] = urlencode(trim($value));
 				$j++;
+
+				// $mystring = 'abc';
+				// $findme   = 'a';
+				// $pos = strpos($value, '"');
+			
+				// echo $value."<br>";
+				// return $pos;
 			}
 
 			$rows[$i] = $newarr;
 		}
-
-
+		// print_r($arr);
+		// return;
+		// return $arr[0]['品名'];
+		// return $rows[0];
 		// array to json
 		$json = json_encode($rows);
 
 		// 再用urldecode把資料轉回成中文格式
-		$json = urldecode($json);		
+		$json = urldecode($json);
 
 		return response($json, 200)
 			->header('Content-Type', 'text/html;charset=big5');
-		
 	}
-
-
 }
--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -3,47 +3,65 @@
 
 @section("content")
 <div id="app">
-    <v-app>
-        <v-main>
-            <v-container>
-              <v-data-table :items="rows" :headers="headers">
-                <template v-slot:item.date="{ item }">
-                  ${item.date.slice(0,10)}
-                </template>
-              </v-data-table>
-            </v-container>
-        </v-main>
-    </v-app>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-text-field v-model="search"></v-text-field>
+        <v-data-table :items="rows" :headers="headers" :search="search">
+          <template v-slot:item.date="{ item }">
+            ${item.date.slice(0,10)}
+          </template>
+        </v-data-table>
+      </v-container>
+    </v-main>
+  </v-app>
 </div>
 @endsection
 
 
 @section("footer")
 <script>
-    new Vue({
-        el: '#app',
-        vuetify: new Vuetify(),
-        delimiters:['${','}'],
-        data: {
-          rows: [],
-          headers: [
-            {text:'ID',value:'id'},
-            {text:'日期',value:'date'},
-            {text:'品名',value:'name'}
-          ]
-
+  new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    delimiters: ['${', '}'],
+    data: {
+      search: '',
+      rows: [],
+      headers: [{
+          text: 'ID',
+          value: 'id'
         },
-        mounted() {
-          axios.get('expense/data',{params:{
-            name: '頂針'
-          }}).then((res)=>{
-            console.log(res.data)
-            this.rows = res.data
-          })
-        },        
-        methods: {
-
+        {
+          text: '日期',
+          value: 'date'
         },
-    })
+        {
+          text: '品名',
+          value: 'name'
+        }
+      ]
+
+    },
+    mounted() {
+      axios.get('expense/data', {
+        // params: {
+        //   name: '頂針'
+        // }
+      }).then((res) => {
+        // json 字串用""括起,如果內容有"符號,會出問題
+        // 要加 \ 跳脫
+        let text = "鋼質傘型頂針 5\"*MT4";
+
+        let position = text.search("Blue");
+        // console.log(position)
+        console.log(res.data)
+        this.rows = res.data
+      })
+    },
+    methods: {
+
+    },
+  })
 </script>
 @endsection
